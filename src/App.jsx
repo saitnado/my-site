fad184b6788355293b2f2cc264482476e43d95c0
Anastasia517 <nastya-andreeva-2018@inbox.ru>
2026-05-21 18:02:45 +0300

    first site

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div style={{
+      textAlign: "center",
+      marginTop: "100px",
+      fontFamily: "Arial"
+    }}>
+      <h1>САЙТ ДВУХ ЕБЛАНОВ</h1>
+
+      <p>
+        оно живое (вроде)
+      </p>
+
+      <button onClick={() => alert("оно работает")}>
+        нажми сюда
+      </button>
+    </div>
+  )
+}
+
+export default App
